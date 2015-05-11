29c92d1b49a203be8e10efabf936da7ed2b002a5
Leo Deng <myst.dg@gmail.com>
2015-05-11 09:22:24 +0800

    improve card component

--- public/dist/components/card.js
+++ public/dist/components/card.js
@@ -1,21 +1,21 @@
-void function(window, document, undefined) {
+void function() {
+  var currentDocument = document.currentScript.ownerDocument
+  function getTemplate(selector) {
+    return currentDocument.querySelector(selector).content.cloneNode(true)
+  }
+
+  var TYPES = {1: '♠', 2: '♥', 3: '♣', 4: '♦'}
+  var VALUES = {2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', a: '10', b: 'J', c: 'Q', d: 'K', e: 'A' }
   var proto = Object.create(HTMLElement.prototype)
 
   proto.createdCallback = function() {
     console.log('create card component')
-    var link = document.querySelector('link.component.card')
-    var template = link.import.querySelector('template')
-    var templateContent = template.content.cloneNode(true)
-
-    var TYPES = {1: '♠', 2: '♥', 3: '♣', 4: '♦'}
-    var VALUES = {2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', a: '10', b: 'J', c: 'Q', d: 'K', e: 'A' }
-
+    var template = getTemplate('template')
     var card = this.dataset.card
     var color = TYPES[card[0]]
     var value = VALUES[card[1]]
-
     var root = this.createShadowRoot()
-    root.appendChild(templateContent)
+    root.appendChild(template)
     root.querySelector('.color').innerHTML = color
     root.querySelector('.value').innerHTML = value
   }
@@ -25,4 +25,4 @@ void function(window, document, undefined) {
   })
 
   window.HTMLTexasCardElement = TexasCard
-}(window, document)
+}()
